5d184a7a9af8f3778f35a7db06ba3498b43d2732
FidiaRahmatunnisa <slayday176@gmail.com>
2024-03-29 03:20:31 +0700

    upload pembayaran service
    ada tambahan pembayaran sebagai client service

--- pembayaran/src/main/java/com/fidia/pembayaran/PembayaranApplication.java
+++ pembayaran/src/main/java/com/fidia/pembayaran/PembayaranApplication.java
@@ -2,8 +2,10 @@ package com.fidia.pembayaran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PembayaranApplication {
 
 	public static void main(String[] args) {
