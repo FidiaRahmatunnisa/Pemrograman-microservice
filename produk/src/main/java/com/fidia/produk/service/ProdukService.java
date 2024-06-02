6baeaa86c7a4c1c4138093c8ee782c6f349d2786
FidiaRahmatunnisa <slayday176@gmail.com>
2024-06-02 18:18:52 +0700

    sedikit perbaikan kode dibagian service , ada beberapa nama  dari entitas yang tidak sesuai,

--- produk/src/main/java/com/fidia/produk/service/ProdukService.java
+++ produk/src/main/java/com/fidia/produk/service/ProdukService.java
@@ -21,62 +21,54 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ProdukService {
     
   @Autowired
-  private ProdukRepository productRepository;
+  private ProdukRepository produkRepository;
 
     @GetMapping
     public List<Produk> getAll(){
-        return productRepository.findAll();
+        return produkRepository.findAll();
     } 
     
-    public Produk getProduct(Long id){
-        return productRepository.findById(id).get();
+    public Produk getProduk(Long id){
+        return produkRepository.findById(id).get();
     }
     
   
-    public Produk getProductById(Long productId){
-        return productRepository.findById(productId).get();
+    public Produk getProdukById(Long produkId){
+        return produkRepository.findById(produkId).get();
 
     }
     
-    public void insert(Produk product){
-        productRepository.save(product);
+    public void insert(Produk produk){
+        produkRepository.save(produk);
     }
     
     @Transactional
-    public void update(Long productId, String kode, String nama, String satuan, double harga){
-        Produk product = productRepository.findById(productId)
+    public void update(Long produkId, String kode, String nama, String satuan, double harga){
+        Produk produk = produkRepository.findById(produkId)
                 .orElseThrow(
-                        () -> new IllegalStateException("Product Tidak ada")
+                        () -> new IllegalStateException("Produk Tidak ada")
                 );
         if(kode != null && kode.length()>0
-            && !Objects.equals(product.getKode(), kode)){
-        product.setKode(kode);
+            && !Objects.equals(produk.getKode(), kode)){
+        produk.setKode(kode);
         }
         if(nama != null && nama.length()>0
-                && !Objects.equals(product.getNama(), nama)){
-            product.setNama(nama);
+                && !Objects.equals(produk.getNama(), nama)){
+            produk.setNama(nama);
         }
         if(satuan != null && satuan.length()>0
-                && !Objects.equals(product.getSatuan(), satuan)){
-            product.setSatuan(satuan);
+                && !Objects.equals(produk.getSatuan(), satuan)){
+            produk.setSatuan(satuan);
         }
         if(harga != 0
-                && !Objects.equals(product.getHarga(), harga)){
-            product.setHarga(harga);
+                && !Objects.equals(produk.getHarga(), harga)){
+            produk.setHarga(harga);
         }
       }
 
-     public void delete(Long productId){
-         productRepository.deleteById(productId);
+     public void delete(Long produkId){
+         produkRepository.deleteById(produkId);
      }
     
-//    public Produk getProduk(Long id) {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-//    }
-
-    public Produk getProduk(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
     
 }
